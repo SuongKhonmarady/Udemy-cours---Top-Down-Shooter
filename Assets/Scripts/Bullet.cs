752ddb1591a18207ae1c1cc91f97e5b35fbc96c1
Suong Khonmarady <maradybor@gmail.com>
2025-05-02 02:37:54 +0700

    Finish folder 6

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] private GameObject bulletImpactFx;
 
     private Rigidbody rb => GetComponent<Rigidbody>();
 
     private void OnCollisionEnter(Collision collision)
     {
-        //rb.constraints = RigidbodyConstraints.FreezeAll;
+        CreateImpactFx(collision);
         Destroy(gameObject);
+    }
+
+    private void CreateImpactFx(Collision collision)
+    {
+        if (collision.contacts.Length > 0)
+        {
+            ContactPoint contact = collision.contacts[0];
+
+            GameObject newImpactFx = 
+                Instantiate(bulletImpactFx, contact.point, Quaternion.LookRotation(contact.normal));
+
+            Destroy(newImpactFx, 1f);
+        }
     }
 }
--- Assets/Scripts/PlayerWeaponController.cs
+++ Assets/Scripts/PlayerWeaponController.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class PlayerWeaponController : MonoBehaviour
 {
+    private const float REFERENCE_BULLET_SPEED = 20;
+    //This is a defefault speed from which our mass formula is derived.
+
     private Player player;
 
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed;
     [SerializeField] private Transform gunPoint;
 
+    
 
     [SerializeField] private Transform weaponHolder;
 
@@ -24,7 +28,10 @@ public class PlayerWeaponController : MonoBehaviour
         GameObject newBullet =
             Instantiate(bulletPrefab, gunPoint.position, Quaternion.LookRotation(gunPoint.forward));
 
-        newBullet.GetComponent<Rigidbody>().velocity = BulletDirection() * bulletSpeed;
+        Rigidbody rbNewBullet = newBullet.GetComponent<Rigidbody>();
+
+        rbNewBullet.mass = REFERENCE_BULLET_SPEED / bulletSpeed;
+        rbNewBullet.velocity = BulletDirection() * bulletSpeed;
 
         Destroy(newBullet, 10);
 
